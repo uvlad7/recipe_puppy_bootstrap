1d1f1c72463c0883ed2693171268ad6a6296eef7
uvlad7 <uvlad7@gmail.com>
2021-08-09 03:32:22 +0300

    YAY - my tests are passing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,8 @@ Capybara.javascript_driver = :selenium_chrome_headless
 # If you want to see the tests actually running in the Chrome browser
 # uncomment the lines below and comment out the above 2 lines
 
-# Capybara.default_driver = :selenium_chrome
-# Capybara.javascript_driver = :selenium_chrome
+Capybara.default_driver = :selenium_chrome
+Capybara.javascript_driver = :selenium_chrome
 
 # Slows down the execution speed of the tests
 # Source - https://stackoverflow.com/a/46840590/908842
